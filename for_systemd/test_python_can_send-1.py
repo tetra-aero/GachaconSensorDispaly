637f70089198acb944e661677d41d64dc5af62b1
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-02-20 17:18:36 +0900

    fix, >SocketcanBus was not properly shut down
    fix, >SocketcanBus was not properly shut down

--- for_systemd/test_python_can_send-1.py
+++ for_systemd/test_python_can_send-1.py
@@ -4,9 +4,10 @@ import can
 interface = 'socketcan'
 channel = 'vcan0'
 
+bus = can.interface.Bus(channel=channel, interface=interface)
+
 def producer(id):
     """:param id: Spam the bus with messages including the data id."""
-    bus = can.interface.Bus(channel=channel, interface=interface)
     for i in range(10):
         msg = can.Message(arbitration_id=0xc0ffee, data=[id, i, 0, 1, 3, 1, 4, 1], is_extended_id=True)
         bus.send(msg)
@@ -15,5 +16,5 @@ def producer(id):
 
 producer(10)
 
-
+bus.shutdown()
 
