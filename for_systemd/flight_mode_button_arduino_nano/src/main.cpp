55ebd7adb1971e1c6b287a8fa6fdd5038af05f86
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-02-21 11:48:40 +0900

    arduino nano every, LED ON/OFF, check to work serial
    arduino nano every, LED ON/OFF, check to work serial

--- for_systemd/flight_mode_button_arduino_nano/src/main.cpp
+++ for_systemd/flight_mode_button_arduino_nano/src/main.cpp
@@ -3,12 +3,16 @@
 const int buttonPin = 2;  // ボタンを接続するピン
 volatile bool buttonPressed = false;  // 割り込み用のフラグ
 
+const int BuildinLEDPin = 13;  // 基板に実装済みのLED
+
 // 割り込みハンドラ（ISR）
 void buttonISR() { 
     buttonPressed = true;
 }
 
 void setup() {
+    pinMode(BuildinLEDPin, OUTPUT); // LEDを出力モードに設定
+
     pinMode(buttonPin, INPUT_PULLUP); // 内部プルアップ抵抗を有効化
     Serial.begin(115200);
 
@@ -22,4 +26,10 @@ void loop() {
         buttonPressed = false; // フラグをリセット
         delay(300); // チャタリング対策
     }
+
+    Serial.println("Hello, World!");
+    digitalWrite(BuildinLEDPin, HIGH); // LEDを点灯
+    delay(500); // 0.5s待つ
+    digitalWrite(BuildinLEDPin, LOW); // LEDを消灯
+    delay(500); // 0.5s待つ
 }
