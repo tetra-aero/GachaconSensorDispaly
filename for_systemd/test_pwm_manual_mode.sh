9a0e3cbdb0f91faff65e8d13a1ed3515a778bac3
Yoshihiro Nakagawa <taku3wzr@gmail.com>
2025-04-03 14:58:39 +0900

    add pwm manual
    add pwm manual

---
+++ for_systemd/test_pwm_manual_mode.sh
@@ -0,0 +1,28 @@
+# test_pwm_manual_mode
+# can1とcan0とcan2は接続に合わせてくれ
+
+cansend can1 00001801#0320
+cansend can1 00001401#40
+cansend can1 00001802#0320
+cansend can1 00001402#40
+cansend can1 00001803#0320
+cansend can1 00001403#40
+cansend can1 00001804#0320
+cansend can1 00001404#40
+cansend can1 00001805#0320
+cansend can1 00001405#40
+cansend can1 00001806#0320
+cansend can1 00001406#40
+
+cansend can0 00001807#0320
+cansend can0 00001407#40
+cansend can0 00001808#0320
+cansend can0 00001408#40
+cansend can0 00001809#0320
+cansend can0 00001409#40
+cansend can0 0000180A#0320
+cansend can0 0000140A#40
+cansend can0 0000180B#0320
+cansend can0 0000140B#40
+cansend can0 0000180C#0320
+cansend can0 0000140C#40
